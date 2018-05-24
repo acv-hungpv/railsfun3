c36d89c49cbd0c04c6000915f6d0e47bc4f8faf1
Hung Phan <hungpv@atmarkcafe.org>
2018-05-24 10:53:56 +0700

    add render

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
-
+  before_action :get_product, only: [:update, :show, :edit, :destroy]
   def index
     @products = Product.where(published: nil)
   end
 
   def show
-    @product = Product.find(params[:id])
+    #@product = Product.find(params[:id])
   end
 
   def new
@@ -30,9 +30,31 @@ class ProductsController < ApplicationController
     render :new
   end
 
+  def edit
+    # @product = Product.find(params[:id])
+    render :new
+  end
+
+  def update
+    #@product = Product.find(params[:id])
+    return redirect_to products_url, notice: 'you have successfully update product' if @product.update(product_params)
+    flash.now[:notice] = 'There is an error in your update form'
+    render :new
+  end
+
+  def destroy
+    return redirect_to products_url, notice: 'you have successfully delete product' if @product.destroy
+    flash.now[:notice] = 'There is an error in your delete form'
+    render :new
+  end 
+
   private
 
   def product_params
     product_params = params.require(:product).permit( :title, :description, :price, :published, :category_id, :country)
+  end
+
+  def get_product
+    @product = Product.find(params[:id])
   end
 end
